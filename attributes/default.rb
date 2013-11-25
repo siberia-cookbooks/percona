ab74a10cbb5c72fcb72dfad69bf29e950bf6aa58
Jacques Marneweck <jacques@powertrip.co.za>
2013-11-25 12:00:50 +0000

    Revert to 5.5.31 on 2013Q2
    Signed-off-by: Jacques Marneweck <jacques@powertrip.co.za>

--- attributes/default.rb
+++ attributes/default.rb
@@ -8,11 +8,11 @@
 #
 
 default['percona'] = {
-  'version'=> '5.6.13',
-  'percona_version' => '56',
+  'version'=> '5.5.31',
+  'percona_version' => '55',
   'ssl' => '1',
   'bind_address' => '127.0.0.1',
-  'service_name' => 'pkgsrc/percona',
+  'service_name' => 'pkgsrc/percona-server',
   'server_root_password' => 'egMacpadvedjifsAsjobGiopwagshErm',
   'key_buffer_size' => '170M',
   'myisam_sort_buffer_size' => '32M',
